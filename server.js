cf52005697000fb13f63ef249089008e34f50b1e
henriqueffazendeiro <henriquefazendeiropy@gmail.com>
2025-01-01 14:02:31 +0000

    Itsdsdsfa

--- server.js
+++ server.js
@@ -427,7 +427,7 @@ app.get(['/pagina-criada/:sessionId', '/*'], async (req, res) => {
                         <iframe 
                             width="100%" 
                             height="80" 
-                            src="https://www.youtube.com/embed/${getYoutubeId(page.pageData.youtubeUrl)}?autoplay=1&controls=1" 
+                            src="https://www.youtube.com/embed/${getYoutubeId(page.pageData.youtubeUrl)}?autoplay=1&mute=0&controls=1&loop=1&playlist=${getYoutubeId(page.pageData.youtubeUrl)}" 
                             frameborder="0" 
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                             allowfullscreen>
