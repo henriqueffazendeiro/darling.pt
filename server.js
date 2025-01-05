5975874cb76a32f280ea88e226d5ccdef4828995
HenriqueFazendeiro <henriquemhvfazendeiro@gmail.com>
2025-01-05 22:37:48 +0000

    Update price dispdfgfdo

--- server.js
+++ server.js
@@ -466,7 +466,7 @@ app.get(['/pagina-criada/:sessionId', '/*'], async (req, res) => {
 
                     #loading-text {
                         color: white;
-                        margin-top: 20px;
+                        margin-top: 80px; // Changed from 20px to 80px
                         font-family: 'Rubik', sans-serif;
                     }
 
